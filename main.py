599876d76452a56c4477292a781e4bc3252daddd
iremcorak <iremcorak@gmail.com>
2024-07-04 09:11:32 +0300

    kameranın başlatılması

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #basler kameradan görüntü alma, kaydetme ve sıcaklık kontrolü
 
 from pypylon import pylon
+
+img = pylon.PylonImage()
+tlf = pylon.TlFactory.GetInstance()
+
+cam = pylon.InstantCamera(tlf.CreateFirstDevice())
+cam.Open()
+cam.StartGrabbing()
