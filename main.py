577de0e91696399c05df38f277914f1a1e5de53a
iremcorak <iremcorak@gmail.com>
2024-07-05 11:01:54 +0300

    grab result buffer

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 
 from pypylon import pylon
 
+num_img_save = 10
 img = pylon.PylonImage()
 tlf = pylon.TlFactory.GetInstance()
 
 cam = pylon.InstantCamera(tlf.CreateFirstDevice())
 cam.Open()
 cam.StartGrabbing()
+
+for i in range(num_img_save):
+    with cam.RetrieveResult(2000) as result:
+
+        img.AttachGrabResultBuffer(result)
