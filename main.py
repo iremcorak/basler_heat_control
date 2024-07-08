b4c46dea82b195d985a70024e6eea15e97ec2938
iremcorak <iremcorak@gmail.com>
2024-07-08 16:38:07 +0300

    image format ve quality set  edildi.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #basler kameradan görüntü alma, kaydetme ve sıcaklık kontrolü
 
 from pypylon import pylon
+import platform
 
 num_img_save = 10
 img = pylon.PylonImage()
@@ -14,3 +15,10 @@ for i in range(num_img_save):
     with cam.RetrieveResult(2000) as result:
 
         img.AttachGrabResultBuffer(result)
+
+        if platform.system() == 'Windows':
+            # The JPEG format that is used here supports adjusting the image
+            # quality (100 -> best quality, 0 -> poor quality).
+            ipo = pylon.ImagePersistenceOptions()
+            quality = 90 - i * 10
+            ipo.SetQuality(quality)
